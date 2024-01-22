9281a31780d05b898445177d5d9beec4e81213f0
tuattia <attiaitem@gmail.com>
2024-01-22 17:30:10 +0800

    make update blog wow

--- process/update_blog.php
+++ process/update_blog.php
@@ -6,7 +6,12 @@ $id = $_SESSION['id_blog'];
 $content = $_POST['content'];
 $category = $_POST['category'];
 $date = $_POST['date'];
-$user_id = $_POST['user_id'];
+
+$user = $_POST['user_id']; //ambil full_name dari form
+//cari id_user di user berdasarkan full_name nya
+$select_name = $DB->query("SELECT * FROM user WHERE LOWER(full_name) LIKE LOWER('%$user%') ORDER BY id_user DESC LIMIT 1 ");
+$data = $select_name->fetch_object();
+$user_id = $data->id_user;
 
 $query = "UPDATE blog SET `content`='$content', `category`='$category', `date`='$date', `user_id`='$user_id' WHERE `id_blog`='$id'";
 $res = $DB->query($query);
