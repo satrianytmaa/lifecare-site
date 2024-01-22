5499f6bda54c54230106bcdb8fc79a7c2bfe9a01
Angelina-a11y <putusri2334@gmail.com>
2024-01-22 17:14:41 +0800

    search id_user by full_name create blog

--- process/create_blog.php
+++ process/create_blog.php
@@ -4,7 +4,12 @@ require('db.php');
 $content = $_POST['content'];
 $category = $_POST['category'];
 $date = $_POST['date'];
-$user_id = $_POST['user_id'];
+
+$user = $_POST['user_id']; //ambil full_name dari form
+//cari id_user di user berdasarkan full_name nya
+$select_name = $DB->query("SELECT * FROM user WHERE LOWER(full_name) LIKE LOWER('%$user%') ORDER BY id_user DESC LIMIT 1 ");
+$data = $select_name->fetch_object();
+$user_id = $data->id_user;
 
 $query = "INSERT INTO blog(content,category,date,user_id) VALUES ('" . $content . "','" . $category . "','" . $date . "','" . $user_id . "')";
 
